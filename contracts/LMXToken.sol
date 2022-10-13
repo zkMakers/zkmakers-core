7af514c15fd4ee190ee45965562e372cd658fce2
Juanjo Chust <nytyr@me.com>
2022-10-13 14:38:03 +0200

    feat: 🎸 Add LMX Token

---
+++ contracts/LMXToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract LMXToken is ERC20 {
+    constructor(uint256 totalSupply_) ERC20("Liquid Miners Token", "LMX") {
+        _mint(msg.sender, totalSupply_);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
