cb61f7508a72abc6539dcb529e2541b5c8000f7a
Juanjo Chust <nytyr@me.com>
2022-12-27 13:00:59 +0100

    feat: 🎸 Change signatures to EIP-712

--- .solcover.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    skipFiles: ["./mocks/Token.sol","./mocks/PairTokenA.sol","./mocks/PairTokenB.sol"],
-    client: require('ganache-cli'),
-  };
--- contracts/IProofVerifier.sol
+++ contracts/IProofVerifier.sol
@@ -4,16 +4,12 @@ pragma solidity 0.8.7;
 interface IProofVerifier {
     /**
     * Checks if the proof is correct and returns the signer. This method should revert if the signer is invalid.
-    * Format of the signature:
-    *   ['address', 'uint256', 'uint256', 'uint256', 'address', 'bytes32'],
-    *   [
-    *       senderAddress,
-    *       totalPoints,
-    *       nonce,
-    *       lastProofTime,
-    *       poolAddress,
-    *       uidHash,
-    *   ]
+    * Format of the signature, using EIP-712:
+    *
+    * EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)
+    * Example: LiquidMiners, 1, chainId, poolAddres
+    *
+    * Proof(address senderAddress,uint256 totalPoints,uint256 nonce,uint256 lastProofTime,address poolAddress,bytes32 uidHash)
     */
     function verify(address sender, uint256 amount, uint256 nonce, uint256 proofTime, address pool, bytes32 uidHash, bytes calldata proof) external view returns (address);
 }
